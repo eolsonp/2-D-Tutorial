256ed32d86c2b4d441dbf163e849639a2d5bc9b9
Eric Olson <Eric.Olson3@iam.local>
2018-10-31 14:54:51 -0500

    up to part 11
    checkpoint stuff

--- 2-D Tutorial/Assets/Scripts/Hazard.cs
+++ 2-D Tutorial/Assets/Scripts/Hazard.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Hazard : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Something entered the hazard");
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered hazard.");
+            PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            player.Respawn();
+            
+        }
+        else
+        {
+            Debug.Log("Something entered the hazard");
+        }
+       
     }
 
 }
