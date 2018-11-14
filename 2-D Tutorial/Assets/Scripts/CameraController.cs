d32a977244f02a781133c7fd6d32030e4f1c4bd5
Eric Olson <Eric.Olson3@iam.local>
2018-11-14 15:15:20 -0600

    added a background scroller

--- 2-D Tutorial/Assets/Scripts/CameraController.cs
+++ 2-D Tutorial/Assets/Scripts/CameraController.cs
@@ -15,11 +15,7 @@ public class CameraController : MonoBehaviour
         offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
 
     void LateUpdate()
     {
