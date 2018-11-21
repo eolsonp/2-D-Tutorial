ecbe362d42c5692c98ddf2cc9bb0db5335339490
Eric Olson <Eric.Olson3@iam.local>
2018-11-21 13:15:20 -0600

    door added, but not in background

---
+++ 2-D Tutorial/Assets/Scripts/Door.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isPlayerInTrigger;
+
+    //cant use this becuase it triggers door twice, player has 2 colliders
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetButtonDown("Activate"))
+    //        {
+    //            Debug.Log("Player activated door!");
+    //        }
+
+    //    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door!");
+        }
+    }
+
+}
