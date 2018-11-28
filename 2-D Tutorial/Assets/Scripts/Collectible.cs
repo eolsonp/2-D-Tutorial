793384a2d497ab9e0c9c3c0bcbfbbb3e49448bf9
Eric Olson <Eric.Olson3@iam.local>
2018-11-28 15:04:34 -0600

    added coin counter

--- 2-D Tutorial/Assets/Scripts/Collectible.cs
+++ 2-D Tutorial/Assets/Scripts/Collectible.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class Collectible : MonoBehaviour {
 
     private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2d;
+    private int coinCount;
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
         
-        {
-            audioSource = GetComponent<AudioSource>();
-        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            coinCount++;
+            Debug.Log(" Coin count: " + coinCount);
             audioSource.Play();
-
-           // Destroy(gameObject);
+            spriteRenderer.enabled = false;
+            boxCollider2d.enabled = false;
+            Destroy(gameObject, audioSource.clip.length);
         }
     }
     // Use this for initialization
