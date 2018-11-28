5a0a72a97ec973a5ff7e0a280dd5c8bd880a56f1
Eric Olson <Eric.Olson3@iam.local>
2018-11-28 14:07:31 -0600

    added collectibles and sprites for animations

---
+++ 2-D Tutorial/Assets/Scripts/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    
+}
