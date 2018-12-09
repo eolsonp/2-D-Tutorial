b2d7948021ef65b57ca428febbcae0337f5d8225
eolsonp <43188502+eolsonp@users.noreply.github.com>
2018-12-08 19:42:08 -0600

    scripts added

---
+++ Collectible.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2d;
+    private static int coinCount;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            coinCount++;
+            Debug.Log(" Coin count: " + coinCount);
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2d.enabled = false;
+            Destroy(gameObject, audioSource.clip.length);
+        }
+    }
+    // Use this for initialization
+    
+}
